eb86ae5988b3e48191ab0acf45c569dbac9954b9
Max Kovpak <max_kovpak@hotmail.com>
2018-11-25 11:25:50 +0200

    added readme

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\File;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    /**
+     * Home page & put file to the queue.
+     */
     public function index()
     {
         return view('index.index');
     }
 
+    /**
+     * View the list of files with statuses & ability to download completed items.
+     */
     public function files()
     {
         $files = File::orderBy('id', 'desc')->get();
